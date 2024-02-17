3a0ccb5520073c87d76ecd531dc7d3533737e736
KarolG <gitesiek@gmail.com>
2024-02-17 02:07:02 +0100

    created a mvp for vboxmanage

---
+++ adapters.py
@@ -0,0 +1,67 @@
+import subprocess
+
+
+class mainManager():
+    def run_command(self, command):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def list_virtual_machines(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def get_vm_info(self, vm_name):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def get_all_vm_status(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def start_vm(self, vm_name):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def stop_vm(self, vm_name):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class VBoxManage(mainManager):
+    def __init__(self):
+        self.vboxmanage = 'D:/vbox/vboxmanage.exe '
+
+    def run_command(self, command):
+        result = subprocess.run(self.vboxmanage + command, shell=True, stdout=subprocess.PIPE, text=True)
+        return result.stdout
+
+    def list_virtual_machines(self):
+        command = "list vms"
+        return self.run_command(command)
+
+    def get_vm_info(self, vm_name):
+        command = "showvminfo " + vm_name
+        return self.run_command(command)
+
+    def get_all_vm_status(self):
+        running_vms = self.get_running_vms()
+        all_vms = self.list_virtual_machines()
+        running_vms = running_vms.splitlines()
+        all_vms = all_vms.splitlines()
+        for vm in all_vms:
+            if vm in running_vms:
+                print(vm, "is running")
+            else:
+                print(vm, "is not running")
+
+    def get_running_vms(self):
+        command = "list runningvms"
+        return self.run_command(command=command)
+
+    def start_vm(self, vm_name):
+        command = "startvm " + vm_name
+        return self.run_command(command)
+
+    def stop_vm(self, vm_name):
+        command = "controlvm " + vm_name + " poweroff"
+        return self.run_command(command)
+
+
+vbox = VBoxManage()
+vbox.start_vm('deb')
+vbox.get_all_vm_status()
+vbox.stop_vm('deb')
